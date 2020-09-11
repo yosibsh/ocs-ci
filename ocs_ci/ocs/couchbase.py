53a589b93f352a0c1c75b5d08e11b8963cca0333
Shreekar <sshreeka@redhat.com>
2020-09-11 16:20:53 +0530

    ocs_ci/ocs/couchbase.py  - Updated create_couchbase_worker() to Use Different SC in external mode
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- ocs_ci/ocs/couchbase.py
+++ ocs_ci/ocs/couchbase.py
@@ -14,7 +14,7 @@ from ocs_ci.ocs import constants
 from ocs_ci.ocs.pillowfight import PillowFight
 from ocs_ci.ocs.ocp import switch_to_default_rook_cluster_project
 from ocs_ci.ocs.resources.pod import get_pod_obj
-from tests.helpers import wait_for_resource_state
+from tests.helpers import wait_for_resource_state, storagecluster_independent_check
 
 log = logging.getLogger(__name__)
 
@@ -180,6 +180,8 @@ class CouchBase(PillowFight):
         logging.info('Creating pods..')
         cb_example = templating.load_yaml(constants.COUCHBASE_WORKER_EXAMPLE)
         cb_example['spec']['servers'][0]['size'] = replicas
+        if storagecluster_independent_check():
+            cb_example['spec']['volumeClaimTemplates'][0]['spec']['storageClassName'] = constants.DEFAULT_EXTERNAL_MODE_STORAGECLASS_RBD
         self.cb_examples = OCS(**cb_example)
         self.cb_examples.create()
 
